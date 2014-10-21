5809daef37ebc614aa83bc818074bfbdcb805648
Jimi Vyas <jimipvyas@gmail.com>
2014-10-21 12:36:29 -0400

    finished user queries with age, name, and displaying birth year for some reason

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,11 @@
+
+today = Time.new
 puts "What is your name?"
 name = gets.chomp
 puts "Hello, #{name}!"
+puts "How old are you?"
+age = gets.chomp
+birth_year = (today.year - age.to_i)
+puts "You were born in #{birth_year}, right?"
+
+
